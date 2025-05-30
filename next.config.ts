f36395081591c38f0f6931bb3eeb312c58d68734
Li Yushen <lysrain21@163.com>
2025-05-30 13:54:04 +0800

    config Github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const repositoryName = '706acc_web'; // <--- 将 'your-repo-name' 替换为你的仓库名
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
+  // 如果你的 GitHub Pages 站点是通过子目录访问的
+  basePath: `/${repositoryName}`,
+  assetPrefix: `/${repositoryName}/`,
 };
 
 export default nextConfig;
